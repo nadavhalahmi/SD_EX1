200e5524ddc843a6581c61901b97f784a06dfaa1
nadavhalahmi <nadavhalahmi@campus.technion.ac.il>
2020-05-19 23:08:41 +0300

    moved KnownPeerComparator to new file

--- coursetorrent-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseTorrent.kt
+++ coursetorrent-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseTorrent.kt
@@ -254,23 +254,6 @@ class CourseTorrent @Inject constructor(private val databases: Databases, privat
         return res.sortedWith(KnownPeerCompartor)
     }
 
-    class KnownPeerCompartor {
-
-        companion object : Comparator<KnownPeer> {
-
-            override fun compare(p1: KnownPeer, p2: KnownPeer): Int = when {
-                p1.ip.split(".")[0] != p2.ip.split(".")[0] ->
-                    p1.ip.split(".")[0].toInt() - p2.ip.split(".")[0].toInt()
-                p1.ip.split(".")[1] != p2.ip.split(".")[1] ->
-                    p1.ip.split(".")[1].toInt() - p2.ip.split(".")[1].toInt()
-                p1.ip.split(".")[2] != p2.ip.split(".")[2] ->
-                    p1.ip.split(".")[2].toInt() - p2.ip.split(".")[2].toInt()
-                else ->
-                    p1.ip.split(".")[3].toInt() - p2.ip.split(".")[3].toInt()
-            }
-        }
-    }
-
     /**
      * Return all known statistics from trackers of the torrent identified by [infohash]. The statistics displayed
      * represent the latest information seen from a tracker.
